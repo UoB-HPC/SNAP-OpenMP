2c5cc32bdb93627f4674b5cd6817a3b8de4e4c91
Matthew Martineau <m.martineau@hotmail.co.uk>
2015-11-24 23:05:45 +0000

    Finalised implementation

--- multi_target/src.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source mod.src
-#sourece /cm/shared/languages/Intel-Compiler-XE-16/compilers_and_libraries_2016.0.109/linux/bin/compilervars.sh
-source /cm/shared/languages/Intel-Compiler-XE-15_Update_2/composer_xe_2015.2.164/bin/compilervars.sh intel64
-#source /cm/shared/languages/Intel-Compiler-XE-15_Update_2/impi/5.0.3.048/mic/bin/mpivars.sh
